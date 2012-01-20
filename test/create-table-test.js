e010ca42df0e2a9e606ea6c745c8635ff13ca07f
Anders Janmyr <anders@janmyr.com>
2012-01-20 15:43:54 +0100

    Started to add headers

--- test/create-table-test.js
+++ test/create-table-test.js
@@ -2,9 +2,17 @@ var vows = require('vows'),
     assert = require('assert');
 
 var DynamoDB = require('dynamo-db');
-var db = new DynamoDB();
+var db = new DynamoDB({key: process.env['S3_KEY']});
 
-vows.describe('Create Table').addBatch({
+vows.describe('DynamoDB').addBatch({
+  'when calling createHeaders with an operation': { 
+    topic: db.createHeaders({ op: 'CreateTable'}),
+
+    'we get an header object back': function(topic) {
+      console.log(topic);
+      assert.isNotNull(topic);
+    }
+  },
   'when calling createTable with no arguments': { 
     topic: function() {
       db.createTable({}, this.callback);
